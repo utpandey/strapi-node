2cdb2dfbc500568fde1e287a885ba996a8e4c000
utpandey <utsavpandey8@gmail.com>
2021-11-30 12:47:36 +0530

    fix: database.js changes for postgre

--- config/database.js
+++ config/database.js
@@ -14,24 +14,24 @@
 //   },
 // });
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'mysql',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'lrm_project'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', 'Utsav@123'),
-        ssl: env.bool('DATABASE_SSL', false),
-      },
-      options: {}
-    },
-  },
-});
+// module.exports = ({ env }) => ({
+//   defaultConnection: 'default',
+//   connections: {
+//     default: {
+//       connector: 'bookshelf',
+//       settings: {
+//         client: 'mysql',
+//         host: env('DATABASE_HOST', '127.0.0.1'),
+//         port: env.int('DATABASE_PORT', 3306),
+//         database: env('DATABASE_NAME', 'lrm_project'),
+//         username: env('DATABASE_USERNAME', 'root'),
+//         password: env('DATABASE_PASSWORD', 'Utsav@123'),
+//         ssl: env.bool('DATABASE_SSL', false),
+//       },
+//       options: {}
+//     },
+//   },
+// });
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
