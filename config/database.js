c990cab7a7ebcadd9af6cdd984336d11cc8fe9f3
utpandey <utsavpandey8@gmail.com>
2021-11-30 16:44:27 +0530

    feat: database.js setup for both postgres dev and prod env

--- config/database.js
+++ config/database.js
@@ -40,17 +40,14 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
+        host: env('PG_DATABASE_HOST', '127.0.0.1'),
+        port: env.int('PG_DATABASE_PORT', 5432),
+        database: env('PG_DATABASE_NAME', 'lrm-project'),
+        username: env('PG_DATABASE_USERNAME', 'postgres'),
+        password: env('PG_DATABASE_PASSWORD', '0000'), // ssl: { rejectUnauthorized: env.bool('PG_DATABASE_SSL_SELF', false), // For self-signed certificates },
       },
       options: {
-        ssl: env.bool('DATABASE_SSL', false),
+        ssl: env.bool('PG_DATABASE_SSL', false),
       },
     },
   },
